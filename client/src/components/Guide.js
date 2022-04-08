a5dfd96b30ea6b692fc94f8ad35ff06c6ddd8de0
sourabh <sourabhvaish007@gmail.com>
2022-04-08 21:55:05 +0530

    guid page some changes

--- client/src/components/Guide.js
+++ client/src/components/Guide.js
@@ -8,19 +8,18 @@ const Guide = () => {
   }, []);
   return (
     <>
-      <div className="container">
-        <div className="con flex justify-center items-center mt-14 ">
-          <div className="main">
-            <h1 className="text-4xl "> Guide</h1>
-            <p className="text-base">
-              You can copy paste the code in your browser console to quickly
-              test FakeEcommerceAPI.
-            </p>
-            <h1 className="text-black text-2xl font-bold my-6">
-              Getting a resource
-            </h1>
-            <h1 className=" font-semibold">For getting all Products</h1>
-            <div className=" mx-auto w-[360px] sm:w-fit ">
+      <div className="container mt-6">
+        <div className="flex justify-center items-center w-full px-4 ">
+          <div className="main ">
+            <div className="w-96 md:w-[560px] px-4 md:px-0">
+              <h1 className="text-4xl "> Guide</h1>
+              <p className="text-base">
+                You can copy paste the code in your browser console to quickly
+                test FakeEcommerceAPI.
+              </p>
+              <h1 className=" font-semibold text-3xl mt-4">
+                For getting all Products
+              </h1>
               <pre className="">
                 <code className="language-js ">
                   {`fetch('https://instaclone.tk/getallproducts')
@@ -29,20 +28,41 @@ const Guide = () => {
                 </code>
               </pre>
             </div>
-            <h1 className="text-black text-2xl font-bold my-6">Output</h1>
-            <div className=" mx-auto w-[360px] sm:w-fit ">
+            <div className="w-96 md:w-[560px] px-4 md:px-0">
+              <h1 className="">👇output</h1>
               <pre className="">
                 <code className="language-js ">
                   {`{
-_id: "625001a3f57dd834e00ca8cc",
-title: "Apple iPhone 12 Pro, 128GB, Graphite",
-description: "Unlocked and compatible with any carrier of choice on GSM and CDMA networks (e.g. AT&T, T-Mobile, Sprint, Verizon, US Cellular, Cricket, Metro, Tracfone, Mint Mobile, etc.). Tested for battery health and guaranteed to come with a battery that exceeds 90% of original capacity. Inspected and guaranteed to have minimal cosmetic damage, which is not noticeable when the device is held at arm's length. Successfully passed a full diagnostic test which ensures like-new functionality and removal of any prior-user personal information. Includes a brand new, generic charging cable that is certified Mfi (Made for iPhone) and a brand new, generic wall plug that is UL certified for performance and safety. Also includes a SIM tray removal tool but does not come with headphones or a SIM card. Backed by a one-year satisfaction guarantee.",
-image: "http://res.cloudinary.com/sourabhvaish/image/upload/q_auto/v1649410223/fsmb8v2pnxldukqw8kq6.jpg",
-price: "$1500",
-category: "mobilephone",
-createdAt: "2022-04-08T09:34:27.957Z",
-updatedAt: "2022-04-08T09:34:27.957Z",
-__v: 0,}`}
+"title": "title",
+"description": "description",
+"image": "image",
+"price": "price",
+"category": "category"}`}
+                </code>
+              </pre>
+            </div>
+            <div className="w-96 md:w-[560px] px-4 md:px-0">
+              <h1 className=" font-semibold text-3xl mt-4">
+                Products by category
+              </h1>
+              <pre className="">
+                <code className="language-js ">
+                  {`fetch('https://instaclone.tk/category/mobiles')
+.then(response => response.json())                                                         
+.then(json => console.log(json))}`}
+                </code>
+              </pre>
+            </div>
+            <div className="w-96 md:w-[560px] px-4 md:px-0">
+              <h1 className="">👇output</h1>
+              <pre className="">
+                <code className="language-js ">
+                  {`{
+"title": "title",
+"description": "description",
+"image": "image",
+"price": "price",
+"category": "category"}`}
                 </code>
               </pre>
             </div>
