e6272a0a6ae5a6484cd002b79a390ce143843f59
sourabh <sourabhvaish007@gmail.com>
2022-04-10 10:55:10 +0530

    db fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "./.env" });
 
 const connectDB = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
